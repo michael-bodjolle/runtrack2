9f3cf779ff8211cf99fcca21318f8b1c923fef9f
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-05-05 15:40:06 +0200

    jour 2 job 1 et 2

---
+++ jour02/job01/index.php
@@ -0,0 +1,20 @@
+<?php
+$nombre=0 ;
+while ($nombre<=1337)
+{
+   if($nombre==42)
+    {
+        echo "<b><u> $nombre</u></b>";
+        
+    }
+    else {
+        echo"$nombre";
+    
+    }
+    echo"<br>";
+    $nombre=$nombre+1;
+    
+}
+
+ 
+?>
---
+++ jour02/job02/index.php
@@ -0,0 +1,19 @@
+<?php
+$nombre = 0 ;
+while ($nombre <= 1337)
+{
+     
+    
+    if ($nombre == 26 || $nombre == 37 || $nombre == 88 || $nombre == 1111 )
+    {
+         
+    }
+    else  
+    {
+        echo $nombre ;
+        echo "<br>" ;
+    }
+   
+    $nombre = $nombre+1;
+}
+?>
