a47324a4a6eca70571a2f8fae819c6411bb373ff
michael-bodjolle <michael.bodjolle@lalateforme.io>
2020-05-06 10:59:04 +0200

    jr 3 job 1

---
+++ jour03/job01/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$array= [200, 204, 173, 98, 171, 404, 459 ] ;
+  foreach ($array as $number) {
+    
+      if ($number% 2 == 0)
+      {
+          echo "$number est paire <br>" ;
+      }
+      else
+      {
+          echo "$number est impaire <br>" ;
+
+    
+      }
+  } 
